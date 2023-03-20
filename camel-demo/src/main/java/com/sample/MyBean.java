fd5beeec68750ae6fc97480a9cdb091c27b1731a
fmarchioni <marchioni.francesco@gmail.com>
2023-03-20 12:28:37 +0100

    fixed issue with mvn repository

--- camel-demo/src/main/java/com/sample/MyBean.java
+++ camel-demo/src/main/java/com/sample/MyBean.java
@@ -26,23 +26,5 @@ public class MyBean {
             kSession.dispose();
         }
     }
-    /*
-     * public void callDrools(String server, int processors, int memory, int diskspace) { KieServices kieServices =
-     * KieServices.Factory.get();
-     *
-     * KieContainer kContainer = kieServices.getKieClasspathContainer();
-     *
-     * System.out.println("Creating kieBase"); KieBase kieBase = kContainer.getKieBase();
-     *
-     * System.out.println("There should be rules: "); for (KiePackage kp : kieBase.getKiePackages()) { for (Rule rule :
-     * kp.getRules()) { System.out.println("kp " + kp + " rule " + rule.getName()); } }
-     *
-     * System.out.println("Creating kieSession"); KieSession session = kieBase.newKieSession();
-     *
-     * System.out.println("Now running data");
-     *
-     * Server s1 = new Server(server, processors, memory, diskspace); session.insert(s1); session.fireAllRules();
-     *
-     * }
-     */
+   
 }
--- camel-demo/src/main/java/com/sample/Resource.java
+++ camel-demo/src/main/java/com/sample/Resource.java
@@ -14,12 +14,27 @@ import javax.ws.rs.core.Response;
 import org.apache.camel.Body;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
+import org.drools.compiler.kie.builder.impl.KieServicesImpl;
+import org.drools.core.event.DefaultAgendaEventListener;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.builder.Message;
+import org.kie.api.builder.ReleaseId;
+import org.kie.api.builder.Results;
+import org.kie.api.definition.KiePackage;
+import org.kie.api.definition.rule.Rule;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 
 @Path("/start")
 public class Resource {
 
     @Inject
     ProducerTemplate producerTemplate;
+
+   
+
+    /* Start Drool Rule through a Camel Route */
 
     @GET
     public Response startCamel() {
@@ -28,4 +43,30 @@ public class Resource {
         return Response.ok("Camel Route started").build();
     }
 
+
+     /* You can also test the Drools Rule from here:
+     @GET
+     @Path("/direct")
+     public Response startRouteDirectly() {
+ 
+         callDrools();
+         return Response.ok("Rule started").build();
+     }
+   
+    public void callDrools() {
+
+        KieServices ks = KieServices.Factory.get();
+        ReleaseId releaseId = ks.newReleaseId("com.sample", "basic-kjar", "1.0.0");
+        KieContainer kContainer = ks.newKieContainer(releaseId);
+        KieSession kSession = kContainer.newKieSession();
+
+        try {
+            Person john = new Person("john", 25);
+            kSession.insert(john);
+            kSession.fireAllRules();
+        } finally {
+            kSession.dispose();
+        }
+    }
+     */
 }
